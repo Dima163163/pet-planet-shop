cd5ff91a2e7e6512ea5f71537aede2bdb14e483b
Dima163163 <khlopovdima@gmail.com>
2024-05-16 12:04:16 +0400

    js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
-const buttons = document.querySelectorAll('.store__category-button');
-
-const changeActiveBtn = (event) => {
-	const target = event.target;
-
-	buttons.forEach((button) => {
-		button.classList.remove('store__category-button_active');
-	});
-
-	target.classList.add('store__category-button_active');
-};
-
-buttons.forEach((button) => {
-	button.addEventListener('click', changeActiveBtn);
-});
-
 const API_URL = `https://puzzling-happy-flier.glitch.me`;
-// /api/products/category
-
+const buttons = document.querySelectorAll('.store__category-button');
 const productList = document.querySelector('.store__list');
+const cartButton = document.querySelector('.store__cart-button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const cartItemsList = document.querySelector('.modal__cart-items');
+const modalCloseButton = document.querySelector('.modal-overlay__close-button');
+const cartCount = cartButton.querySelector('.store__cart-cnt');
 
 // Создание карточки товара
-const createProductCard = (product) => {
-	console.log('product: ', product);
+const createProductCard = ({ photoUrl, name, price }) => {
 	const productCard = document.createElement('li');
 	productCard.classList.add('store__item');
 
 	productCard.innerHTML = `
 		<article class="store__product product">
-			<img class="product__image" src="${API_URL}/${product.photoUrl}" alt=${product.name} width="388" height="261">
-			<h3 class="product__title">${product.name}</h3>
-			<p class="product__price">${product.price}&nbsp;₽</p>
+			<img class="product__image" src="${API_URL}/${photoUrl}" alt=${name} width="388" height="261">
+			<h3 class="product__title">${name}</h3>
+			<p class="product__price">${price}&nbsp;₽</p>
 			<button class="product__btn-add-cart">
 				Заказать
 			</button>
@@ -65,4 +52,73 @@ const fetchProductByCategory = async (category) => {
 	}
 };
 
-fetchProductByCategory('Домики');
+const changeCategory = ({ target }) => {
+	const category = target.textContent;
+
+	buttons.forEach((button) => {
+		button.classList.remove('store__category-button_active');
+	});
+
+	target.classList.add('store__category-button_active');
+	fetchProductByCategory(category);
+};
+
+buttons.forEach((button) => {
+	button.addEventListener('click', changeCategory);
+	if (button.classList.contains('store__category-button_active')) {
+		console.log(button.textContent);
+		fetchProductByCategory(button.textContent);
+	}
+});
+
+const renderCartItems = () => {
+	cartItemsList.textContent = '';
+	const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+	cartItems.forEach((item) => {
+		const listItem = document.createElement('li');
+
+		listItem.textContent = item;
+		cartItemsList.append(listItem);
+	});
+};
+
+cartButton.addEventListener('click', () => {
+	modalOverlay.style.display = 'flex';
+	renderCartItems();
+});
+
+modalOverlay.addEventListener('click', ({ target }) => {
+	if (
+		target === modalOverlay ||
+		target.closest('.modal-overlay__close-button')
+	) {
+		modalOverlay.style.display = 'none';
+	}
+});
+
+const updateCartCount = () => {
+	const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+	cartCount.textContent = cartItems.length;
+};
+updateCartCount();
+
+const addToCart = (productName) => {
+	const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+	console.log('cartItems: ', cartItems);
+	cartItems.push(productName);
+	localStorage.setItem('cartItems', JSON.stringify(cartItems));
+	updateCartCount();
+};
+
+productList.addEventListener('click', ({ target }) => {
+	if (target.closest('.product__btn-add-cart')) {
+		const productCard = target.closest('.store__product');
+		console.log('productCard: ', productCard);
+		const productName =
+			productCard.querySelector('.product__title').textContent;
+		console.log(productName);
+		addToCart(productName);
+	}
+});
